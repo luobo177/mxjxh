fd034190a19a7fc50befeb45d2a2f80554c4a9ec
罗博 <14852764+luoboxiangqianchong@user.noreply.gitee.cominit>
2024-10-22 16:53:27 +0800

    完成了log页面的登录逻辑，后面可能还要改

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logRoutes = require('./log');
 const app = express();
-
+const path = require('path');
 // 提供 frontend 文件夹作为静态文件目录
-app.use(express.static('frontend'));
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// 直接返回 log.html 文件作为根目录访问
+
 
 // 解析 application/json 请求体
 app.use(bodyParser.json());
